fac2e798e068e932a366d347d7234e34df73cf59
Gustavo-Souza-Coutinho <gustavo.couty@hotmail.com>
2024-02-26 20:29:23 -0300

    Initial commit

---
+++ src/test/java/com/meuprimeirosite/demo/MeuPrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meuprimeirosite.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
